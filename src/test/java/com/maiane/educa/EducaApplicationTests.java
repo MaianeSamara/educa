0af521d0bfcee84888b7b298339a493bda65f741
MaianeSamara <maianesamara@gmail.com>
2022-05-28 20:17:41 -0300

    Iniciado Projeto EDUCA IMEPAC

---
+++ src/test/java/com/maiane/educa/EducaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiane.educa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

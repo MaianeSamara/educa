185c9dfe441c6af8960110c6b3e782485ccaaa53
MaianeSamara <maianesamara@gmail.com>
2022-06-03 21:16:36 -0300

    Modelo Usuário

---
+++ src/main/java/com/maiane/educa/modelo/Usuario.java
@@ -0,0 +1,62 @@
+package com.maiane.educa.modelo;
+
+import java.sql.Date;
+
+public class Usuario {
+
+	private Integer id;
+	private String nome;
+	private String email;
+	private String senha;
+	private Date dataInclusao;
+	private Date dataAlteracao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Date getDataInclusao() {
+		return dataInclusao;
+	}
+
+	public void setDataInclusao(Date dataInclusao) {
+		this.dataInclusao = dataInclusao;
+	}
+
+	public Date getDataAlteracao() {
+		return dataAlteracao;
+	}
+
+	public void setDataAlteracao(Date dataAlteracao) {
+		this.dataAlteracao = dataAlteracao;
+	}
+
+}
